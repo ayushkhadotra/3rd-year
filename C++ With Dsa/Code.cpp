2241d1bb018a1363de13b1c8b1d244096062d667
ayushkhadotra <ayush.23b0231058@abes.ac.in>
2026-02-28 18:41:07 +0530

    C++w Dsa

---
+++ C++ With Dsa/Code.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*int Age =21;
+    char grade = 'A';
+    float PI = 3.14f;
+    double price = 99.99;
+
+    bool isSafe = true;
+    cout<<"My name is Ayush\n My age is "<<sizeof(Age)<<"\n My grade is "<<grade<<"\n My PI is "<<PI<<"\n Price is "<<price<<"\n Is Safe? "<<isSafe<<"\n";
+    */
+  
+   /*char b = 'a';
+   int value = b;
+   double money = 101.99;
+   int newmoney = (int )money;
+   cout << newmoney << "\n";
+    cout<<"The value of a is "<< value <<"\n The value of b is "<<b ;
+    */
+   /*int age;
+   cout << " enter your age : ";
+   cin >> age;
+   cout << "ur age is : " << age << endl;
+    */
+
+    // Day -2 Calculator Program 
+   
+   int a,b;
+   
+   cout << "Enter two numbers: ";
+   cin >> a >> b;
+   cout << "sum = " << (a+b) << endl; 
+   cout << "difference = " << (a-b) << endl;    
+   cout << "product = " << (a*b) << endl; 
+   cout << "quotient = " << (a/b) << endl; 
+   cout << "modulo = " << (a%b) << endl; 
+   cout << "average = " << (a+b)/2.0 << endl;  
+   cout << "maximum = " << (a>b ? a : b) << endl; 
+   cout << "minimum = " << (a<b ? a : b) << endl;
+   cout << "power = " << pow(a,b) << endl; 
+  
+   return 0;
+}
+// boilerplate code for C++ program
+
+    
