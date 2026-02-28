0bd5f6b0b88176a1da443b6f141af72251d76d40
ayushkhadotra <ayush.23b0231058@abes.ac.in>
2026-02-28 19:35:30 +0530

    f

--- C++ With Dsa/Condition/Conditional.cpp
+++ C++ With Dsa/Condition/Conditional.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    int n = 9;
+    int n = -9;
     if (n >=0){
         cout << "n is a pos:" << endl;
         }
     else {
-        cout << "n is a neg:" << endl;
+        cout << "n is a neg:\n" ;
     }
     return 0;
     }    
