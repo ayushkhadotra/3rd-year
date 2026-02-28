49cc98df5105a6443403175f7c98ad94e5c9af1b
ayushkhadotra <ayush.23b0231058@abes.ac.in>
2026-02-28 19:01:25 +0530

    Conditional loops

---
+++ C++ With Dsa/Conditional.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 9;
+    if (n >=0){
+        cout << "n is a pos:" << endl;
+        }
+    else {
+        cout << "n is a neg:" << endl;
+    }
+    return 0;
+    }    
