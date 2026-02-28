2eb6f6061b94cb93e3825d782d94fa435abbf92a
ayushkhadotra <ayush.23b0231058@abes.ac.in>
2026-02-28 20:09:33 +0530

    settlement

--- C++ With Dsa/Condition/upperor lower/Contents/lwupp.cpp
+++ C++ With Dsa/Condition/upperor lower/Contents/lwupp.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
+    /*
     char ch;
     cout << "Enter a character: " << endl;
     cin >> ch;
     if (ch  >= 'a' && ch <+ 'z' && ch <= 'z'){
         cout << "ch is a lowercase letter:" << endl;
     }
-    else if (ch >= 'A' && ch <= 'Z'){
+    else if (ch >= 65 && ch <= 90){
         cout << "ch is an uppercase character\n";
     }
     else {
         cout << "ch is not an alphabet:" << endl;
     }
+
+    */
+
+    int n = 9;
+    cout << (n>=0?"pos":"neg") << endl;
     return 0;
 }
